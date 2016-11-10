47287e6061c6caa0917ec29ac8fbe1efe267450c
joshuaburgos18 <markjoshuaburgos26@gmail.com>
2016-11-10 12:43:39 +0800

    Update GUI on options

--- src/States/OptionState.java
+++ src/States/OptionState.java
@@ -1,8 +1,10 @@
 package States;
 
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
@@ -10,6 +12,10 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class OptionState extends BasicGameState{
+	
+	Image start;
+	Image leaderboard;
+	Image howToPlay;
 	
 	public void enter(GameContainer gc, StateBasedGame s) throws SlickException {
 	
@@ -22,16 +28,27 @@ public class OptionState extends BasicGameState{
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame s, Graphics g) throws SlickException {
-		//Color the background
-		g.setColor(Color.white);
-		g.fillRect(0, 0, States.GAME_HEIGHT, States.GAME_WIDTH);
-		
+		//Color the background		
+		g.drawString("Hello Player!", 330, 50);
+		g.drawString("Survive and shoot at others while", 230, 100);
+		g.drawString("trying to keep your own tank alive!", 230, 130);
+		start = new Image("resources/start.png");
+		leaderboard = new Image("resources/leaderboard.png");
+		howToPlay = new Image("resources/howToPlay.png");
+		g.drawImage(start, 330, 170);
+		g.drawImage(leaderboard, 150, 300);
+		g.drawImage(howToPlay, 500, 300);
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame s, int t) throws SlickException {
-		if (gc.getInput().isKeyPressed(Input.KEY_ENTER)) {			
-			s.enterState(States.GAME);
+		int posX = Mouse.getX();
+		int posY = Mouse.getY();
+		
+		if ((posX>335 && posX<425) && (posY>175 && posY<265)) {			
+			if(Mouse.isButtonDown(0)){
+				s.enterState(States.GAME);
+			}
 		}
 	}
 
